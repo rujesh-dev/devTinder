e6a88c5b4ffa2683713e22ae226c1e1a480ea2e5
rujesh <rujesh@afreespace.com>
2025-11-18 08:34:41 +0530

    created an express server

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/',(req, res)=>{
+    res.send('Hello devs......');
+})
+
+app.listen(3000, ()=>{
+    console.log('server is created successfully');
+    
+})
+ 
